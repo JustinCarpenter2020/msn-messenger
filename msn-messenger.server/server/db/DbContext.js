7a745442dabbd2da6599bb532d9fa24b83130c2d
Harrison Wheeler <wheelerharrison93@gmail.com>
2021-05-05 11:34:58 -0600

    message schema - linked to dbcontext - added files

--- msn-messenger.server/server/db/DbContext.js
+++ msn-messenger.server/server/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import ValueSchema from '../models/Value'
 import AccountSchema from '../models/Account'
+import MessageSchema from '../models/Message'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Message = mongoose.model('Message', MessageSchema)
 }
 
 export const dbContext = new DbContext()
