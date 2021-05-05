f5b1d820f334cc4531b8f42a899daa6300bab3ad
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-05-05 17:30:21 -0600

    css

--- msn-messenger.server/server/models/Account.js
+++ msn-messenger.server/server/models/Account.js
@@ -8,6 +8,7 @@ const Account = new Schema(
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
     picture: { type: String }
+    // default: 'http://2.bp.blogspot.com/_r1kMibaacEs/TLVQgzYP33I/AAAAAAAAJXk/j8T-F70lTQ8/s320/Windows+Live+Messenger+2011+v15.4.3502.922+FINAL+%28Espa%C3%B1ol%29.jpg' for picture?
     // NOTE If you wish to add additional public properties for Accounts do so here
   },
   { timestamps: true, _id: false, toJSON: { virtuals: true } }
